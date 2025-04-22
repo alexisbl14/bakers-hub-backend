2ddced32105299fe2395dd799b6c66e95b217159
alexisbl14 <ablydon@ucdavis.edu>
2025-04-21 19:02:00 -0700

    add models for recipes and a join table of recipe and ingredient

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+from inventory.models import Ingredient
 
 # Create your models here.
+class Recipe(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    servings = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True) # track when recipe was created
+
+        # to display object nicely
+    def __str__(self):
+        return f"{self.name} ({self.servings} servings)"
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name="ingredients")
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    amount = models.FloatField()
+    unit = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"{self.amount} {self.unit} of {self.ingredient.name} in {self.recipe.name}"
