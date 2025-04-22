a4034f5c86262dba0c82d90fa6d6cc1097f961e6
alexisbl14 <ablydon@ucdavis.edu>
2025-04-22 08:25:27 -0700

    create views for recipe

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Recipe, RecipeIngredient
+from .serializers import RecipeIngredientSerializer, RecipeSerializer
 
 # Create your views here.
+class RecipeCreateView(generics.ListCreateAPIView):
+    serializer_class = RecipeSerializer
+    queryset = Recipe.objects.all()
+    # ensure only users logged in can access the view
+    permission_classes = [permissions.IsAuthenticated]
+
+    # specify what user to be assigned
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
