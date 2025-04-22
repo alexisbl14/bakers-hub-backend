7330829ec128acb17ed43feff623875abbe3cf55
alexisbl14 <ablydon@ucdavis.edu>
2025-04-22 12:30:35 -0700

    add total cost and cost per serving to serializer for recipes

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Recipe, RecipeIngredient
 from inventory.models import Ingredient
+from decimal import Decimal, InvalidOperation
 
 class RecipeIngredientSerializer(serializers.ModelSerializer):
     ingredient_name = serializers.ReadOnlyField(source='ingredient.name')
@@ -11,10 +12,12 @@ class RecipeIngredientSerializer(serializers.ModelSerializer):
 
 class RecipeSerializer(serializers.ModelSerializer):
     ingredients = RecipeIngredientSerializer(many=True)
+    total_cost = serializers.SerializerMethodField()
+    cost_per_serving = serializers.SerializerMethodField()
 
     class Meta:
         model = Recipe
-        fields = ['id', 'name', 'description', 'servings', 'created_at', 'ingredients']
+        fields = ['id', 'name', 'description', 'servings', 'created_at', 'ingredients', 'total_cost', 'cost_per_serving']
 
     def create(self, validated_data):
         ingredients_data = validated_data.pop('ingredients')
@@ -24,3 +27,21 @@ class RecipeSerializer(serializers.ModelSerializer):
             RecipeIngredient.objects.create(recipe=recipe, **item)
 
         return recipe
+
+    def get_total_cost(self, obj):
+        total = 0
+        for item in obj.ingredients.all():
+            try:
+                # Converting to decimal before calculations
+                unit_cost = (Decimal(str(item.amount)) / Decimal(str(item.ingredient.quantity))) * item.ingredient.cost
+                total += unit_cost
+            except (ZeroDivisionError, InvalidOperation, AttributeError):
+                continue
+        return round(total, 2)
+
+    def get_cost_per_serving(self, obj):
+        try:
+            # Converting to decimal before calculations
+            return round(self.get_total_cost(obj) / Decimal(str(obj.servings)), 2)
+        except (ZeroDivisionError, InvalidOperation):
+            return 0
