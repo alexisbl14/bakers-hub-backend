73e2c76fc5adb1a0a412f464eb4cde2e4d2a062f
alexisbl14 <ablydon@ucdavis.edu>
2025-04-22 13:38:56 -0700

    add a view to calculate profit margin

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,8 @@
 from rest_framework import generics, permissions
 from .models import Recipe, RecipeIngredient
 from .serializers import RecipeIngredientSerializer, RecipeSerializer
+from decimal import Decimal, InvalidOperation
+from rest_framework.response import Response
 
 # Create your views here.
 class RecipeListCreateView(generics.ListCreateAPIView):
@@ -23,3 +25,21 @@ class RecipeDetailView(generics.RetrieveUpdateDestroyAPIView):
     def get_queryset(self):
         # only show ingredients for logged in user
         return Recipe.objects.filter(user=self.request.user)
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        data = serializer.data.copy()
+
+        # Get optional profit margin input
+        margin = request.query_params.get("margin")
+        if margin:
+            try:
+                margin_decimal = Decimal(str(margin))
+                total_cost = Decimal(str(data.get("total_cost", 0)))
+                suggested_price = round(total_cost * (1 + margin_decimal), 2)
+                data["suggested_price"] = float(suggested_price)
+            except (InvalidOperation, ValueError):
+                data["suggested_price"] = "Invalid margin"
+
+        return Response(data)
